7609f6314643e2b5ec62de032cb5f2974a8dfd62
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2024-06-05 12:04:52 +0200

    convert app.js to .ts

--- app.ts
+++ app.ts
@@ -1,22 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError = require('http-errors');
+import express = require('express');
+import path = require('path');
+import cookieParser = require('cookie-parser');
+import logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/user');
+import cors = require('cors');
+import connectDB = require("./database/database");
+
+const userRouter = require('./routes/user');
 const circuitRouter = require('./routes/circuit');
-const vehicleRouter = require('./routes/vehicle')
-const connectDB = require("./database/database");
+const vehicleRouter = require('./routes/vehicle');
 
 connectDB.default();
 
-var app = express();
-
-const cors = require('cors');
-
-app.use(cors());
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,8 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/circuit', circuitRouter);
 app.use('/vehicle', vehicleRouter);
